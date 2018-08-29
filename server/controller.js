c88087306b0c3c449da83727b4ba42b3454d72fb
Katie McCulloch <katiee_mcculloch@yahoo.com>
2018-08-28 19:23:10 -0700

    setting up db

---
+++ server/controller.js
@@ -0,0 +1,11 @@
+module.exports = {
+    fetch: (req, res) => {
+        console.log('in GET...');
+    },
+    post: (req, res) => {
+        console.log('in POST...');
+    },
+    delete: (req, res) => {
+        console.log('in DELETE...');
+    },
+}
